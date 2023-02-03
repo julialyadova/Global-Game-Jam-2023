e9cd9d776ba55a5e54daecded0cd28b8790595dd
Julia Lyadova <andreeva.js.direct@gmail.com>
2023-02-03 23:33:17 +0500

    Spear Throwing Fix

--- Assets/Scripts/Spear/SpearConfiguration.cs
+++ Assets/Scripts/Spear/SpearConfiguration.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
 public class SpearConfiguration : MonoBehaviour
 {
+    public Transform Head;
+    public float HeadRaycastDistance = 1;
 
-    [FormerlySerializedAs("CenterOfAMass")] 
-    public Vector3 centerOfAMass;
+    private Rigidbody _rigidbody;
+    private bool _hit;
 
     void Start()
     {
-        gameObject.GetComponent<Rigidbody>().centerOfMass = centerOfAMass;
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 45, ForceMode.Impulse);
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.centerOfMass = Head.position;
+    }
+    
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(Head.position, transform.TransformDirection(Vector3.forward), out var hitInfo, HeadRaycastDistance))
+        {
+            Hit();
+        }
+
+        if(!_hit && _rigidbody.velocity != Vector3.zero)
+            _rigidbody.rotation = Quaternion.LookRotation(_rigidbody.velocity);  
+    }
+
+    void Hit()
+    {
+        _hit = true;
+        _rigidbody.isKinematic = true;
     }
 }
