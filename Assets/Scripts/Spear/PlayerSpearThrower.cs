bf5e92c4d0bbbf35967c3ab65398150c28fa24ba
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2023-02-04 16:26:02 +0500

    Update PlayerSpearThrower.cs

--- Assets/Scripts/Spear/PlayerSpearThrower.cs
+++ Assets/Scripts/Spear/PlayerSpearThrower.cs
@@ -2,52 +2,53 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 public class PlayerSpearThrower : NetworkBehaviour
 {
     [FormerlySerializedAs("SpearPrefab")] 
     public GameObject spearPrefab;
-    
-    [FormerlySerializedAs("ForceGrowSpeed")] 
-    public float forceGrowSpeed = 18f;
-    
-    [FormerlySerializedAs("MinForce")] 
-    public float minForce = 3f;
-    
-    [FormerlySerializedAs("MaxForce")] 
-    public float maxForce = 20f;
-    
-    private float Force = 0;
 
+    [FormerlySerializedAs("ThrowsPerSecond")] 
+    public float throwsPerSecond = 1;
+    public float Force = 20f;
+    private bool _canThrow = false;
+
+    private Transform _shootPoint;
+    
+    void Start()
+    {
+        StartCoroutine(ThrowASpearRoutine());
+        _shootPoint = transform;
+    }
+    
     void Update()
     {
-        if (!IsOwner)
-            return;
-
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && IsOwner)
         {
-            if (Force >= minForce)
+            if (IsServer)
             {
-                if (IsServer)
-                {
-                    SpawnSpear();
-                }
-                else
-                {
-                    SpawnSpearServerRpc();
-                }
+                SpawnSpear();
             }
-            Force = 0;
-        }
-
-        if (Input.GetKey(KeyCode.Mouse0) && Force <= maxForce)
-        {
-            Force += Time.deltaTime * forceGrowSpeed;
-            if (Force > maxForce) Force = maxForce;
-            
-            Debug.Log(Force);
+            else
+            {
+                SpawnSpearServerRpc();
+            }
         }
     }
     
+    private IEnumerator ThrowASpearRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1 / throwsPerSecond);
+            _canThrow = true;
+        }
+    }
+
     private void SpawnSpear()
     {
         var spear = Instantiate(spearPrefab, transform.position, transform.rotation);
@@ -63,3 +64,68 @@ public class PlayerSpearThrower : NetworkBehaviour
         SpawnSpear();
     }
 }
+//
+// public class PlayerSpearThrower : NetworkBehaviour
+// {
+//     [FormerlySerializedAs("SpearPrefab")] 
+//     public GameObject spearPrefab;
+//     
+//     [FormerlySerializedAs("ForceGrowSpeed")] 
+//     public float forceGrowSpeed = 18f;
+//     
+//     [FormerlySerializedAs("MinForce")] 
+//     public float minForce = 3f;
+//     
+//     [FormerlySerializedAs("MaxForce")] 
+//     public float maxForce = 20f;
+//     
+//     private float Force = 0;
+//
+//     void Update()
+//     {
+//         if (!IsOwner)
+//             return;
+//
+//         
+//         Force = 20;
+//         
+//         if (Input.GetKeyUp(KeyCode.Mouse0))
+//         {
+//             if (Force >= minForce)
+//             {
+//                 if (IsServer)
+//                 {
+//                     SpawnSpear();
+//                 }
+//                 else
+//                 {
+//                     SpawnSpearServerRpc();
+//                 }
+//             }
+//             Force = 0;
+//         }
+//
+//         if (Input.GetKey(KeyCode.Mouse0) && Force <= maxForce)
+//         {
+//             Force += Time.deltaTime * forceGrowSpeed;
+//             if (Force > maxForce) Force = maxForce;
+//             
+//             Debug.Log(Force);
+//         }
+//     }
+//     
+//     private void SpawnSpear()
+//     {
+//         var spear = Instantiate(spearPrefab, transform.position, transform.rotation);
+//         var networkObject = spear.GetComponent<NetworkObject>();
+//         networkObject.Spawn();
+//         
+//         spear.GetComponent<Rigidbody>().AddForce(transform.forward * Force, ForceMode.Impulse);
+//     }
+//
+//     [ServerRpc]
+//     private void SpawnSpearServerRpc()
+//     {
+//         SpawnSpear();
+//     }
+// }
